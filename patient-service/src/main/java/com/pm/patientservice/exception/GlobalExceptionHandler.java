f94d9d757754c1590e3a85c16cd671a9525277bf
Md Fahad Hasan <mdashfahad@gmail.com>
2024-12-08 20:59:54 +0100

    add emailalreadyexisstexception

--- patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.pm.patientservice.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +13,8 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    //private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         Map<String, String> errors = new HashMap<>();
@@ -20,5 +24,13 @@ public class GlobalExceptionHandler {
         });
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistException.class)
+    public ResponseEntity<String> handleEmailAlreadyExistException(EmailAlreadyExistException exception) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Email address already exist in the system");
+
+        return ResponseEntity.badRequest().body(errors.toString());
     }
 }
