07a7c0a16d89f3068aebf1fc1ec18120432c64cf
Md Fahad Hasan <mdashfahad@gmail.com>
2025-01-13 16:19:25 +0100

    init billing-service module

---
+++ billng-service/src/test/java/com/pm/billngservice/BillngServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.billngservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillngServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
